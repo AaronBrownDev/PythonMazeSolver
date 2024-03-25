7cb4a0df3483b006d851a65d952d8d92703eb4ff
AaronBrownDev <aaronb954@gmail.com>
2024-03-25 08:45:01 -0500

    add point and line class

--- display.py
+++ display.py
@@ -18,6 +18,29 @@ class Window():
         self.__running = True
         while self.__running:
             self.redraw()
+            
+    def draw_line(self, line, fill_color : str):
+        line.draw(self.__canvas, fill_color)
         
     def close(self):
         self.__running = False
+        
+        
+class Point():
+    
+    def __init__(self, x=0, y=0) -> None:
+        self.x = x
+        self.y = y
+        
+
+class Line():
+    
+    def __init__(self, p1 : Point, p2 : Point) -> None:
+        self.point_one = p1
+        self.point_two = p2
+    
+    def draw(self, canvas : Canvas, fill_color : str):
+        canvas.create_line(
+           self.point_one.x, self.point_one.y, self.point_two.x, self.point_two.y, fill=fill_color, width=2
+        )
+        canvas.pack(fill=BOTH, expand=1)
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from display import Window
+from display import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    
+    point_one = Point(0, 0)
+    point_two = Point(50, 50)
+    
+    win.draw_line(Line(point_one, point_two), "blue")
+    
     win.wait_for_close()
     
 main()
